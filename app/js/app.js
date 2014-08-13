f37981a46f9924dc977986cead60a29802a2daa4
James Ward <james@jamesward.org>
2014-08-13 10:39:00 -0600

    switch to forcejs

--- app/js/app.js
+++ app/js/app.js
@@ -1,50 +1,35 @@
-var apiVersion = 'v30.0',
-    clientId = 'YOUR_CONSUMER_KEY_HERE',
-    loginUrl = 'https://login.salesforce.com/',
-    redirectURI = "http://localhost:5000/oauthcallback.html",
-    proxyURL = 'https://sfdc-cors.herokuapp.com';
+$(function() {
 
-function login() {
-    var url = loginUrl + 'services/oauth2/authorize?display=popup&response_type=token' +
-        '&client_id=' + encodeURIComponent(clientId) +
-        '&redirect_uri=' + encodeURIComponent(redirectURI);
-    popupCenter(url, 'login', 700, 600);
-}
+  force.init({
+    appId: '3MVG9xOCXq4ID1uE5sILNJHk06inAqUxj9yjE.FWXLLbWBDt9RNgCnnQF4twGikbdC.nzjvVT9mumyYBoQzPw',
+    proxyURL: 'https://sfdc-cors.herokuapp.com'
+  });
 
-function oauthCallback(response) {
-    if (response && response.access_token) {
-        window.salesforceToken = response.access_token;
-    } else {
-        alert("AuthenticationError: No Token");
-    }
-}
 
-function executeQuery() {
-    if (!window.salesforceToken) {
-        alert('You are not authenticated. Please login first.');
+  $('#btn-login').click(function() {
+    force.login(
+      function() {
+        console.log('Login succeeded');
+      },
+      function(error) {
+        alert('Login failed: ' + error);
+      }
+    );
+  });
+
+  $('#btn-exec').click(function() {
+    if (force.isLoggedIn) {
+      force.query($('#query').val(),
+        function (data) {
+          $('#result').html(JSON.stringify(data, undefined, 3));
+        },
+        function (error) {
+          alert("Error: " + JSON.stringify(error));
+        });
     }
     else {
-      $.ajax(proxyURL + '/services/data/v30.0/query/?q=' + $('#query').val(), {
-        headers: {'Authorization': 'Bearer ' + window.salesforceToken},
-        dataType: 'json',
-        success: function (data) {
-            $('#result').html(JSON.stringify(data, undefined, 3));
-        },
-        error: function (error) {
-            alert("Error: " + JSON.stringify(error));
-        }
-      });
+      alert('You are not authenticated. Please login first.');
     }
-}
+  });
 
-$('#btn-login').click(login);
-$('#btn-exec').click(executeQuery);
-
-function popupCenter(url, title, w, h) {
-    // Handles dual monitor setups
-    var parentLeft = window.screenLeft ? window.screenLeft : window.screenX;
-    var parentTop = window.screenTop ? window.screenTop : window.screenY;
-    var left = parentLeft + (window.innerWidth / 2) - (w / 2);
-    var top = parentTop + (window.innerHeight / 2) - (h / 2);
-    return window.open(url, title, 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-}
+});
